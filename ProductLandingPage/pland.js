712672f634d962aaa2ba06eed53a4c8582ca97b6
Gayathri Harikumar <gayathri.hk175@gmail.com>
2022-08-24 19:45:57 +0530

    Added the landing page for product page

---
+++ ProductLandingPage/pland.js
@@ -0,0 +1,29 @@
+var index = 0;
+var slides = document.querySelectorAll(".slides");
+var dot = document.querySelectorAll(".dot");
+
+function changeSlide(){
+
+  if(index<0){
+    index = slides.length-1;
+  }
+  
+  if(index>slides.length-1){
+    index = 0;
+  }
+  
+  for(let i=0;i<slides.length;i++){
+    slides[i].style.display = "none";
+    dot[i].classList.remove("active");
+  }
+  
+  slides[index].style.display= "block";
+  dot[index].classList.add("active");
+  
+  index++;
+  
+  setTimeout(changeSlide,2000);
+  
+}
+
+changeSlide();
