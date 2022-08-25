cecf4a050a3586abb4dc848f9af302101f7eb0dc
Gayathri Harikumar <gayathri.hk175@gmail.com>
2022-08-25 16:07:25 +0530

    prod page started

---
+++ MainProductPage/prod.js
@@ -0,0 +1,205 @@
+var DataDisplayed = [
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/V07221/herbatol-plus-combo-skin-hygiene-personal-travel-hygiene-kit-2-1641791259.jpg",
+      info: "Herbatol Plus Skin Hygiene Personal...",
+      price: 470.00,
+      category: "Home Hygiene Essentials",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/C64606/beurer-ft-85-non-contact-thermometer-2-1632782626.jpg",
+      info: "Beurer Ft 85 Non Contact Thermometer",
+      price: 2004.80,
+      category: "Medical Equipments",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/Q83701/pharmeasy-all-in-one-vaporizer-1-1654233812.jpg",
+      info: "PharmEasy All-In-One Vaporizer",
+      price: 269.55,
+      category: "Medical Equipments",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/I35347/dabur-laxmi-vilas-ras-tab-10s-2-1632776668.jpg",
+      info: "Dabur Laxmi Vilas Ras Tab 10s",
+      price: 24.0,
+      category: "Pills",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/F31479/halyard-lavender-nitrile-powder-free-hand-gloves-box-of-50-pairs-m-size-2-1642498990.jpg",
+      info: "Jalyard Lavendar Nitrile Powder-Free Hand..",
+      price: 1800.00,
+      category: "Medical Equipments",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/N82504/dr-vaidyas-allergy-and-cold-pack-allergic-pills-24-x-4-herbofit-30-capsules-sung-ho-10g-2-1641792828.jpg",
+      info: "Dr Vaidyas Allergy and Cold Pack",
+      price: 616,
+      category: "Pills",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/T60720/pharmeasy-multivitamin-multimineral-immunity-booster-complete-nutrition-bottle-of-60-2-1658316798.jpg",
+      info: "PharmEasy Multivitamin Multimineral",
+      price: 299,
+      category: "Health Supplements",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/D92359/everherb-by-pharmeasy-amla-juice-rich-in-vitamin-c-natural-immunity-booster-1-litre-bottle-2-1654169607.jpg",
+      info: "Everherb by PharmEasy",
+      price: 199.50,
+      category: "Health Supplements",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/O33816/herbatol-plus-all-purpose-hygiene-spray-with-9999-germ-protection-75ml-2-1641791612.jpg",
+      info: "Herbatol Plus All Purpose Hygiene Spray",
+      price: 147.25,
+      category: "Home Hygiene Essentials",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/P02621/dr-vaidyas-herbokold-powder-50-gm-2-1641791387.jpg",
+      info: "Dr Vaidya's Herbokold Powder 50 Gm",
+      price: 30,
+      category: "Health Supplements",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/B33674/tana-professional-apesin-sdr-san-1l-2-1641791102.jpg",
+      info: "Tana Professional Apesin Sdr San-1",
+      price: 1075.00,
+      category: "Health Supplements",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/S40272/natxtra-amla-cz-immunity-booster-capsules-1x-60-capsules-2-1641793956.jpg",
+      info: "Natxtra Amla-Cz-Immunity",
+      price: 381.50,
+      category: "Health Supplements",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/U82798/swisse-ultibiotic-daily-immune-probiotic-for-immunity-gut-health-30-n-capsules-2-1641789789.jpg",
+      info: "Swisse Ultibiotic Daily Immune Probiotic",
+      price: 2810,
+      category: "Health Supplements",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/R89100/good-home-divine-and-harmony-spray-room-air-freshner-2-x-160-ml-2-1641819776.jpg",
+      info: "Good Home Divine and Harmony Spray",
+      price: 739,
+      category: "Home Hygiene Essentials",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/X59429/green-care-professional-biobact-scent-500ml-2-1641791103.jpg",
+      info: "Greencare Professional Biobact Scent",
+      price: 389,
+      category: "Home Hygiene Essentials",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/131335/omron-ne-c28-c1-nebulizer-vapourizer-2-1641787643.jpg",
+      info: "Omron Nebulizer & Vaporizer",
+      price: 2606,
+      category: "Medical Equipments",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/L51963/serjun-gloves-65-1-1632781574.jpg",
+      info: "Serjun Gloves 6.5",
+      price: 65.60,
+      category: "Masks & Gloves",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/U50771/pulsy-pulse-oximeter-1-2-1641791806.jpg",
+      info: "Pulsy Pulse Oxymeter",
+      price: 3450,
+      category: "Medical Equipments",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/A89747/livinguard-fitness-mask-unbeatable-breathability-ultra-comfort-washable-reusable-steel-blue-l-2-1641793340.jpg",
+      info: "Living Guard Fitness Mask",
+      price: 1892,
+      category: "Masks & Gloves",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/J36816/livinguard-pro-mask-3-layers-95-filtration-washable-reusable-cotton-face-mask-l-black-2-1641792974.jpg",
+      info: "Living Guard Pro Mask",
+      price: 1347,
+      category: "Masks & Gloves",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/K62472/soulflower-essential-oil-breathe-easy-15ml-2-1641792603.jpg",
+      info: "Sunflower Essential Oil Breathe Easy",
+      price: 360,
+      category: "Home Hygiene Essentials",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/Q33667/healthvit-vasaka-powder-250-mg-60-capsules-2-1641790008.jpg",
+      info: "Healthvit Vasaka Powder",
+      price: 444,
+      category: "Health Supplements",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/B91863/healthkart-immunity-supplement-60-tablets-2-1643100604.jpg",
+      info: "Healthkart Immunity Supplements",
+      price: 360,
+      category: "Health Supplements",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/C28625/zoneblue-3-in-one-steam-vaporizer-2-1632782640.jpg",
+      info: "Zone blue 3-in-1 vaporiser",
+      price: 190,
+      category: "Medical Equipments",
+    },
+    {
+      image_url:"https://cdn01.pharmeasy.in/dam/products_otc/H72860/swisse-high-strength-apple-cider-vinegar-tablets-supports-digestion-immunity-30-tabs-2-1641792136.jpg",
+      info: "Swisse High Strength Apple Cider Vinegar",
+      price: 901,
+      category: "Health Supplements",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/206524/naselin-nasal-solution-bottle-of-10-ml-3-1639030531.jpg",
+      info: "Naselin Nasal Solution",
+      price: 64,
+      category: "Health Supplements",
+    },
+    {
+      image_url: "https://cdn01.pharmeasy.in/dam/products_otc/N83864/beco-natural-floor-liquid-1l-baby-pets-safe-2-1644239892.jpg",
+      info: "Beco Natural Floor Liquid",
+      price: 227,
+      category: "Home Hygiene Essentials",
+    },
+  ];
+
+
+
+var cart = JSON.parse(localStorage.getItem("cart")) || [] ;
+
+function displayProducts(DataDisplayed) {
+    DataDisplayed.forEach(function(elem){
+
+        var productDiv = document.createElement("div");
+        productDiv.setAttribute("id","prodBox");
+
+        var productVisual = document.createElement("img");
+        productVisual.setAttribute("id","prodImage");
+        productVisual.src = elem.image_url;
+
+        var productDesc = document.createElement("p");
+        productDesc.setAttribute("id","prodDesc");
+        productDesc.innerText = elem.info;
+
+        var productAmount = document.createElement("p");
+        productAmount.setAttribute("id","prodAmt");
+        productAmount.innerText = elem.price;
+
+        var productButton = document.createElement("button");
+        productButton.setAttribute("id","prodBtn");
+        productButton.innerText = "Add to Cart";
+        productButton.addEventListener("click",function(){
+          addToCart(elem);
+        })
+
+        productDiv.append(productVisual,productDesc,productAmount,productButton);
+        document.querySelector("#covid_prod").append(productDiv);
+    });
+}
+
+displayProducts(DataDisplayed);
+
+function addToCart(elem){
+  cart.push(elem);
+  localStorage.setItem("cart",JSON.stringify(cart));
+}
