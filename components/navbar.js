5e8fd050f420d2294a12bc9c18fd5a198e71eba8
nitinkrs <nitinkrs123@gmail.com>
2022-08-28 11:33:29 +0530

    updation

--- components/navbar.js
+++ components/navbar.js
@@ -3,8 +3,8 @@ let navbar = () =>{
     <div class="navbar_container">
         <div class="navbar">
             <div>
-            <div>
-           <img src="https://assets.pharmeasy.in/web-assets/dist/fca22bc9.png" alt="">
+            <div >
+         <a href="../homepage/index.html"> <img src="https://assets.pharmeasy.in/web-assets/dist/fca22bc9.png" alt="" height="50px";width="100px"></a>
             </div>
                 <hr>
             <div class="nav_add">
@@ -21,25 +21,25 @@ let navbar = () =>{
                 <h3>Download App</h3>
             </div> -->
             <div>
-                <a href="" id="user-details" style ="color: white;">Hello,Log In</a>
+                <a href="../signup/signup.html" id="user-details" style ="color: white;">Hello,Log In</a>
             </div>
             <div>
-               <span><i class="fa-solid fa-percent" style ="color: white;"></i></span><a href="" style ="color: white;">Offers</a>
+               <span><i class="fa-solid fa-percent" style ="color: white;"></i></span><a href="../Offers/offers.html" style ="color: white;">Offers</a>
             </div>
             <div>
-                <span><i class="fa-solid fa-cart-shopping" style ="color: white;"></i></span><a href="" style ="color: white;">Cart</a>
+                <span><i class="fa-solid fa-cart-shopping" style ="color: white;"></i></span><a href="../Cart/Cart.html" style ="color: white;">Cart</a>
             </div>
         </div>
     
     <div class="sub_nav">
-        <div><a href="">Medicine</a></div>
-        <div><a href="">Lab Test</a></div>
+        <div><a href="../Medicine/medicine.html">Medicine</a></div>
+        <div><a href="../Lab Test/lab_test.html">Lab Test</a></div>
         <div><a href="../ProductLandingPage/pland.html">Healthcare</a></div>
-        <div><a href="">Health Blogs</a></div>
-        <div><a href="">PLUS</a></div>
-        <div><a href="">Offers</a></div>
-        <div><a href="">Surgeries</a></div>
-        <div><a href="">Value Store</a></div>
+        <div><a href="../">Health Blogs</a></div>
+        <div><a href="../plus/plus.html">PLUS</a></div>
+        <div><a href="../Offers/offers.html">Offers</a></div>
+        <div><a href="../surgeries/surgery.html">Surgeries</a></div>
+        <div><a href="../Prod/prod.html">Value Store</a></div>
     </div>
     </div>
     `;
